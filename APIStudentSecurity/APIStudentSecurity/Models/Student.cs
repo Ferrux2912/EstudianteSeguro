827ee6cf0df5a18781979ea66e4f6313a15d2667
Sebastian Ferrufino Manyari <a2019117321@estudiantes.upsa.edu.bo>
2020-08-29 13:03:01 -0400

    Proyecto Base con modelo

---
+++ APIStudentSecurity/APIStudentSecurity/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace APIStudentSecurity.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Required]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+
+        [StringLength(30, ErrorMessage = "The field {0} must contain between {2} and {1} characters", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yy}", ApplyFormatInEditMode = false)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
