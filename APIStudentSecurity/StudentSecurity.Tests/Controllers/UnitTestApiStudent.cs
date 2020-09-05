5154b262b239fc43250177fbd309a368b7b7fd90
Sebastian Ferrufino Manyari <a2019117321@estudiantes.upsa.edu.bo>
2020-09-05 13:17:44 -0400

    Se creo el UnitTest del Api web Students

---
+++ APIStudentSecurity/StudentSecurity.Tests/Controllers/UnitTestApiStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using APIStudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSecurity.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTestApiStudent
+    {
+        [TestMethod]
+        public void TestGetStudent()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController();
+
+            //Act
+            var listaEstudiantes = studentsController.GetStudents();
+
+            //Assert
+            Assert.IsNotNull(listaEstudiantes);
+        }
+    }
+}
